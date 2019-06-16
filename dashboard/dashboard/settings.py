618438e597749cbd7a1a5798f5ad5c68db29d7b9
mitus1212 <36314619+mitus1212@users.noreply.github.com>
2019-06-16 16:21:44 +0200

    Update settings.py

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'scraping',
         'USER': 'postgres',
-        'PASSWORD': 'mat123',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
